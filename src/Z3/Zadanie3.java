e53bdbffb7413a605034f801c5d2b8c0962529c9
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2022-02-14 10:53:47 +0100

    zadanie  3

--- src/Z3/Zadanie3.java
+++ src/Z3/Zadanie3.java
@@ -1,12 +1,13 @@
 package Z3;
 import java.io.File;
+import java.util.Arrays;
 
 public class Zadanie3 {
     public static void main(String[] args){
         String path = "C:/Users/Karolina/Desktop";
         File[] files = fileList(path);
         for(File s: files){
-            System.out.println(s);
+            System.out.println(s.getName());
         }
     }
     public static File[] fileList(String path){
