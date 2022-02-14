5220973f5fcad139fb452cf733d69ca976a900e5
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2022-02-14 10:41:16 +0100

    zadanie  2

--- src/Z2/Zadanie2.java
+++ src/Z2/Zadanie2.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public class Zadanie2 {
     public static void main(String[] args){
-        ArrayList lista = new ArrayList();
+        ArrayList<Integer> lista = new ArrayList<>();
         lista.add(123);
         lista.add(234);
         lista.add(345);
         lista.add(456);
+
+        ArrayList<String> lista3 = new ArrayList<>();
+        lista3.add("A");
+        lista3.add("B");
+        lista3.add("C");
+        lista3.add("D");
+        lista3.add("E");
 
         HashSet<String> lista2 = new HashSet<>();
         lista2.add("Ala");
@@ -20,6 +27,7 @@ public class Zadanie2 {
 
         wypiszCoDrugi(lista);
         wypiszCoDrugi(lista2);
+        wypiszCoDrugi(lista3);
     }
     public static <E extends Iterable> void wypiszCoDrugi(E t){
         Iterator<?> iterator = t.iterator();
